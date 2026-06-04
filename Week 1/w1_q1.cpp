54a2c1a922f83302e4482b1b500f63292bf0ea01
Atharva <atharvalad12345@gmail.com>
2026-06-04 18:19:51 +0530

    Submit_Week_1

--- Week 1/w1_q1.cpp
+++ Week 1/w1_q1.cpp
@@ -6,14 +6,14 @@ int main() {
     cin >> n;
 
     long long sum = n * (n + 1) / 2;
-    long long x, givenSum = 0;
+    long long x, sumGiven = 0;
 
     for (long long i = 0; i < n - 1; i++) {
         cin >> x;
-        givenSum += x;
+        sumGiven += x;
     }
 
-    cout << sum - givenSum << endl;
+    cout << sum - sumGiven << endl;
 
     return 0;
 }
