0eb6296be5c5df09963c8bb9fc3be4c753826c63
Akshit_Anand <akshitanand008@gmail.com>
2026-07-12 16:55:12 +0530

    Commit on 2026-07-12 16:55

---
+++ Week 4/new.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+using vint = vector<int>;
+
+template <typename T>
+void vprint(T& V) {
+    for (auto& i : V) { cout << i << " "; }
+    cout << "\n";
+}
+
+template <typename T>
+void pprint(T& P) {
+    for (auto& i : P) { cout << i.first() << " " << i.second() << "\n"; }
+    cout << "\n";
+}
+
+int main() {}
